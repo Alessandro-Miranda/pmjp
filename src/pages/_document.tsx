eddc0f0f7790b9df1755f958e596377c4bc595a8
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-23 20:19:31 -0300

    style: create custom next Document to link to google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;1,600&family=Yanone+Kaffeesatz:wght@400;600&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
