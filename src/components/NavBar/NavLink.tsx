68ce66b1373d04713e08359a235a76ef33c00fa0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-25 01:56:20 -0300

    feat: create custom MenuNavigation.Link

--- src/components/NavBar/NavLink.tsx
+++ src/components/NavBar/NavLink.tsx
@@ -1,26 +1,40 @@
 import { Link as NavigationLink } from '@radix-ui/react-navigation-menu';
 import Link from 'next/link';
 import React from 'react';
+import styles from './navlink.module.scss';
 
 type LinkProps = {
-    /**
-     * Anchor link to internal or external page
-     */
-    href: string;
-    /**
-     * Label or some other children to be rendered inside the link
-     */
-    children: React.ReactNode;
+  /**
+   * Anchor link to internal or external page
+   */
+  href: string;
+  /**
+   * Label or some other children to be rendered inside the link
+   */
+  children: React.ReactNode;
+  /**
+   * Complementary style class. It must be a space-separated string.
+   */
+  className?: string;
+  /**
+   * Defines if navigation link should has border property.
+   */
+  hasBorder?: boolean;
 };
 
-function NavLink({ children, href }: LinkProps) {
+function NavLink({ className, children, href, hasBorder }: LinkProps) {
   return (
     <Link href={href} passHref>
-      <NavigationLink>
+      <NavigationLink
+        className={`${styles.navLink} ${className}`}
+        data-border={hasBorder}
+      >
         {children}
       </NavigationLink>
     </Link>
   );
 }
+
+NavLink.defaultProps = { className: '', hasBorder: false };
 
 export default NavLink;
