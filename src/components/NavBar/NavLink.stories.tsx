156e79935b56354017c4c1c72ca97c74f4e782fb
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-25 01:56:48 -0300

    test(stories): create NavLink story

---
+++ src/components/NavBar/NavLink.stories.tsx
@@ -0,0 +1,44 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import NavLink from './NavLink';
+
+export default {
+  title: 'Components/NavLink',
+  component: NavLink,
+  parameters: {
+    docs: {
+      description: {
+        component: `Custom navigation link.
+            This custom component is composing with \`<NavigationMenu.Link />\`
+            to ensure accessibility and consistent keyboard controll.`,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <NavigationMenu.Root>
+        <NavigationMenu.List>
+          <NavigationMenu.Item>
+            <Story />
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+    ),
+  ],
+} as ComponentMeta<typeof NavLink>;
+
+const Template: ComponentStory<typeof NavLink> = (args) => <NavLink {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  href: '/contato',
+  children: 'contato',
+};
+
+export const LinkWithBorder = Template.bind({});
+LinkWithBorder.args = {
+  href: '/informação',
+  children: 'informação',
+  hasBorder: true,
+};
