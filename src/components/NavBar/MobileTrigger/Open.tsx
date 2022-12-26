e8118291fb79e5d1ce30457d93965d1a68c4db31
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-26 12:08:16 -0300

    feat: create open menu mobile trigger

---
+++ src/components/NavBar/MobileTrigger/Open.tsx
@@ -0,0 +1,27 @@
+import Button from '@Components/Button';
+import menu from '@Icons/menu.svg';
+import Image from 'next/image';
+
+type Props = {
+  handleOpenFn: () => void;
+  className?: string;
+}
+
+function Open({ handleOpenFn, className }: Props) {
+  return (
+    <Button
+      handleClickFn={handleOpenFn}
+      className={className}
+    >
+      <Image
+        src={menu}
+        alt="Abrir menu"
+        fill
+      />
+    </Button>
+  );
+}
+
+Open.defaultProps = { className: '' };
+
+export default Open;
