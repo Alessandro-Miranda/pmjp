c94ce5ea6f090f36df4fe91d5cf3e6e5b4c781ce
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-26 12:39:30 -0300

    test(stories): create CloseTrigger component story

---
+++ src/components/NavBar/MobileTrigger/CloseTrigger.stories.tsx
@@ -0,0 +1,50 @@
+import { expect, jest } from '@storybook/jest';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { Close } from '.';
+import styles from '../styles.module.scss';
+
+export default {
+  title: 'Components/NavBar/MobileTrigger/Close',
+  component: Close,
+  args: {
+    handleCloseFn: jest.fn(),
+    className: styles.navigation__closeList,
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: 'Mobile Trigger component. This component is responsible by closing menu component',
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{
+        background: '#9c0701',
+        position: 'relative',
+        width: '100px',
+        height: '75px',
+      }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Close>;
+
+const Template: ComponentStory<typeof Close> = (args) => <Close {...args} />;
+
+export const Default = Template.bind({});
+
+export const Clicked = Template.bind({});
+Clicked.play = async ({
+  args,
+  canvasElement,
+}) => {
+  const canvas = within(canvasElement);
+
+  userEvent.click(canvas.getByTestId('button'));
+
+  await waitFor(() => expect(args.handleCloseFn).toHaveBeenCalled());
+};
