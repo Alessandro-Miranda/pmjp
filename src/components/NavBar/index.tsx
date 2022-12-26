b496a22d475e4c3eaa1393e0b0c6059b250df469
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-26 15:14:52 -0300

    test: add data-testid to Menu List

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,28 @@
+import * as MobileTrigger from '@Components/NavBar/MobileTrigger';
 import NavLink from '@Components/NavBar/NavLink';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { useState } from 'react';
-import MobileTrigger from './MobileTrigger';
-import styles from './navBar.module.scss';
+import styles from './styles.module.scss';
 
 type Props = {
     /**
      * Menu items that will be rendered
      */
     items: { href: string; label: string; hasBorder?: boolean }[];
+}
+
+function MenuItems({ items }: Props) {
+  return (
+    <>
+      {items.map(({ href, label, hasBorder = false }) => (
+        <NavigationMenu.Item key={label}>
+          <NavLink href={href} hasBorder={hasBorder}>
+            {label}
+          </NavLink>
+        </NavigationMenu.Item>
+      ))}
+    </>
+  );
 }
 
 function NavBar({ items }: Props) {
@@ -19,39 +33,20 @@ function NavBar({ items }: Props) {
 
   return (
     <NavigationMenu.Root className={styles.navBar__container}>
-      <MobileTrigger handleClickFn={handleOpenMenu} />
+      <MobileTrigger.Open
+        handleOpenFn={handleOpenMenu}
+        className={styles.trigger__open__buton}
+      />
       <NavigationMenu.List
         className={styles.navigation__list}
         data-mobile-open={isOpenMenu}
+        data-testid="menu-list"
       >
-        <button
-          type="button"
+        <MobileTrigger.Close
+          handleCloseFn={handleCloseMenu}
           className={styles.navigation__closeList}
-          onClick={handleCloseMenu}
-        >
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
-              fill="currentColor"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-            />
-
-          </svg>
-        </button>
-        {items.map(({ href, label, hasBorder = false }) => (
-          <NavigationMenu.Item key={label}>
-            <NavLink href={href} hasBorder={hasBorder}>
-              {label}
-            </NavLink>
-          </NavigationMenu.Item>
-        ))}
+        />
+        <MenuItems items={items} />
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
