515c5ead5a1cd480f6b79c9b2ccf258ba933af87
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-26 12:08:37 -0300

    feat: export Cose and Open triggers

--- src/components/NavBar/MobileTrigger/index.tsx
+++ src/components/NavBar/MobileTrigger/index.tsx
@@ -1,32 +1,4 @@
-import menu from '@Icons/menu.svg';
-import Image from 'next/image';
-import React from 'react';
-import styles from './mobileTrigger.module.scss';
+import Close from './Close';
+import Open from './Open';
 
-type Props = {
-  handleClickFn: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  className?: string;
-}
-
-function MobileTrigger({ handleClickFn, className }: Props) {
-  return (
-    <div className={`${styles.trigger__container} ${className}`}>
-      <button
-        type="button"
-        aria-label="Abrir menu"
-        data-testid="button-open-menu"
-        onClick={handleClickFn}
-      >
-        <Image
-          src={menu}
-          alt="Abrir menu"
-          fill
-        />
-      </button>
-    </div>
-  );
-}
-
-MobileTrigger.defaultProps = { className: '' };
-
-export default MobileTrigger;
+export { Close, Open };
