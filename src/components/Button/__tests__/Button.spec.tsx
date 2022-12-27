dd421e8a29473ac9b98e3838131f2edfcbe32cb9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-27 09:52:07 -0300

    test: finish button tests

--- src/components/Button/__tests__/Button.spec.tsx
+++ src/components/Button/__tests__/Button.spec.tsx
@@ -1,16 +1,39 @@
 import { beforeEach, describe, test } from '@jest/globals';
 import { composeStories } from '@storybook/testing-react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import * as Stories from './Button.stories';
 
 describe('Button', () => {
   beforeEach(cleanup);
 
-  const { Default } = composeStories(Stories);
+  const { Default, Clicked } = composeStories(Stories);
 
   test('Button should be in the document', () => {
-    const { getByTestId } = render(<Default />);
+    const { getByTestId } = render(<Default {...Default.args} />);
 
-    expect(getByTestId('button')).toBeInTheDocument();
+    expect(getByTestId('story-test')).toBeInTheDocument();
+    expect(getByTestId('button-children')).toBeInTheDocument();
+  });
+
+  test('When button is clicked, it should invoke the onClick handler', () => {
+    const { getByTestId } = render(<Clicked {...Clicked.args} />);
+
+    const button = getByTestId('story-test');
+
+    fireEvent.click(button);
+    expect(button.childElementCount).toBe(1);
+    expect(Clicked.args?.handleClickFn).toHaveBeenCalled();
+  });
+
+  test('Should invoke Story\'s play function', async () => {
+    const DivRoot = document.createElement('div');
+    render(
+      <Clicked {...Default.args} />,
+      { container: DivRoot },
+    );
+
+    await Clicked.play({ args: { ...Clicked.args }, canvasElement: DivRoot });
+
+    expect(Clicked.args?.handleClickFn).toHaveBeenCalled();
   });
 });
