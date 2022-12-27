490da5a925a59fe8c844e1eaa6ada68e63ffc517
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-27 09:50:18 -0300

    test: update style mock

--- __mocks__/styleMock.js
+++ __mocks__/styleMock.js
@@ -1,7 +1,1 @@
-const path = require('path');
-
-module.exports = {
-  process(scss, scssPath) {
-    return { code: `module.exports = ${JSON.stringify(path.basename(scssPath))}` };
-  },
-};
+module.exports = {};
