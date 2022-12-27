876b934cfb029cbc250657ee9458eb32949e1052
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-26 21:16:34 -0300

    test: add React UMD to global scopre and add @storybook/testing-react config

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,3 +1,8 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { setGlobalConfig } from '@storybook/testing-react';
 import '@testing-library/jest-dom';
+import React from 'react';
+import * as globalStorybookConfig from './.storybook/preview';
 
+global.React = React;
+
+setGlobalConfig(globalStorybookConfig);
