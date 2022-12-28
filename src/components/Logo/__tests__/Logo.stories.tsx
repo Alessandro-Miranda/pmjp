0f3e6bd625fa09fde8f2ca814e37cf8b303570b1
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-27 21:05:43 -0300

    test(stories): add component description

--- src/components/Logo/__tests__/Logo.stories.tsx
+++ src/components/Logo/__tests__/Logo.stories.tsx
@@ -4,6 +4,13 @@ import Logo from '../index';
 export default {
   title: 'Components/Logo',
   component: Logo,
+  parameters: {
+    docs: {
+      description: {
+        component: 'This component is responsible for present the church logo on the screen',
+      },
+    },
+  },
 } as ComponentMeta<typeof Logo>;
 
 export const Default: ComponentStory<typeof Logo> = (args) => <Logo />;
