a420abdedcb74dc7e556638d520d75e7fd419b50
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-30 17:06:37 -0300

    feat: add navbar separator and create hero content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Logo from '@Components/Logo';
 import NavBar from '@Components/NavBar';
+import Typography from '@Components/Typography';
+import * as Separator from '@radix-ui/react-separator';
 import styles from '@Styles/Home.module.scss';
 
 export default function Home() {
@@ -16,6 +18,28 @@ export default function Home() {
               { href: '#contact', label: 'contato', hasBorder: true },
             ]}
           />
+        </div>
+
+        <Separator.Root
+          className={styles.header__navBarSeparator}
+          orientation="horizontal"
+        />
+        <div className={styles.header__hero__container}>
+          {/* botão */}
+          { /** Texto decoration */}
+          <blockquote cite="https://twitter.com/pontifex_pt/status/417623614901334016">
+            <Typography className={styles.header__hero__citation}>
+              “ No rosto do
+              {' '}
+              <Typography variant="span">Menino Jesus</Typography>
+              , contemplamos o rosto de Deus. Vinde, adoremos! “
+            </Typography>
+
+            <Typography className={styles.header__hero__citation__author}>
+              - papa francisco
+            </Typography>
+          </blockquote>
+          <div role="banner" aria-hidden className={styles.header__hero__banner} />
         </div>
       </header>
       <main />
