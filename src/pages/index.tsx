f0dab40c9b031071815cdc51ebd45486b4342067
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-12-31 13:00:09 -0300

    feat: create about church scroll section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import ArticleNumber from '@Components/ArticleNumber';
 import Logo from '@Components/Logo';
 import NavBar from '@Components/NavBar';
 import Typography from '@Components/Typography';
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 import * as Separator from '@radix-ui/react-separator';
 import styles from '@Styles/Home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <meta name="theme-color" content="#9c0701" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#9c0701"></meta>
+    </Head>
       <header className={styles.header}>
         <div className={styles.header__navbar}>
           <Logo />
@@ -21,12 +28,10 @@ export default function Home() {
         </div>
 
         <Separator.Root
-          className={styles.header__navBarSeparator}
+          className={styles.content__separator}
           orientation="horizontal"
         />
         <div className={styles.header__hero__container}>
-          {/* botão */}
-          { /** Texto decoration */}
           <blockquote cite="https://twitter.com/pontifex_pt/status/417623614901334016">
             <Typography className={styles.header__hero__citation}>
               “ No rosto do
@@ -39,10 +44,68 @@ export default function Home() {
               - papa francisco
             </Typography>
           </blockquote>
+          
           <div role="banner" aria-hidden className={styles.header__hero__banner} />
         </div>
       </header>
-      <main />
+      <main>
+        <section className={styles.section}>
+          <Typography variant='h1' className={styles.section__title}>
+            conheça nossa paróquia
+          </Typography>
+          <Separator.Root
+            orientation='horizontal'
+            className={styles.content__separator}
+          />
+
+          <ScrollArea.Root className={styles.section__scrollArea__root}>
+            <ScrollArea.Viewport className={styles.section__scrollArea__viewport}>
+              <article className={styles.section__article}>
+                <ArticleNumber number={1} />
+                <div className={styles.section__article__text__container}>
+                  <Typography variant='h2' className={styles.section__article__title}>
+                    sobre a paróquia
+                  </Typography>
+
+                  <Typography>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </Typography>
+                </div>
+              </article>
+              
+              <article className={styles.section__article}>
+                <ArticleNumber number={2} />
+                <div className={styles.section__article__text__container}>
+                  <Typography variant='h2' className={styles.section__article__title}>
+                    missas
+                  </Typography>
+
+                  <Typography>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </Typography>
+                </div>
+              </article>
+              
+              <article className={styles.section__article}>
+                <ArticleNumber number={3} />
+                <div className={styles.section__article__text__container}>
+                  <Typography variant='h2' className={styles.section__article__title}>
+                    localização
+                  </Typography>
+
+                  <Typography>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </Typography>
+                </div>
+              </article>
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar orientation='vertical'>
+              <ScrollArea.ScrollAreaThumb />
+            </ScrollArea.Scrollbar>
+          </ScrollArea.Root>
+
+        </section>
+      </main>
     </>
   );
 }
