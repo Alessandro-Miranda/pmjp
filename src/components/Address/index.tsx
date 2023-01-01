97adf3654ec6fd96f79b3c42e7d600d403f60a73
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-01 19:10:59 -0300

    feat: create address component used on Church location and community location

---
+++ src/components/Address/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+import Typography from '@Components/Typography';
+import Location from '@Icons/location.svg';
+import styles from './styles.module.scss';
+
+type Props = {
+  address: string;
+  icon?: string;
+  iconClass?: string;
+  textClass?: string;
+}
+
+function Address({ address, icon, iconClass, textClass }: Props) {
+  return (
+    <div>
+      <Image
+        src={icon ?? Location}
+        alt="Localização"
+        className={`${styles.location__icon} ${iconClass ?? ''}`}
+      />
+      <Typography className={`${styles.location__text} ${textClass ?? ''}`}>
+        {address}
+      </Typography>
+    </div>
+  );
+}
+
+Address.defaultProps = { icon: null, iconClass: null, textClass: null };
+
+export default Address;
