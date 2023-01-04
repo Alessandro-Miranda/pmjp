aaabf296a2a516e5d1f72ff4c0758f627be9507d
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-04 12:25:36 -0300

    test: add data-testid to component

--- src/components/PastoralCard/index.tsx
+++ src/components/PastoralCard/index.tsx
@@ -3,9 +3,21 @@ import Image, { StaticImageData } from 'next/image';
 import styles from './styles.module.scss';
 
 type Props = {
+  /**
+   * Card title
+   */
   title: 'acolhida' | 'catequese' | 'música' | 'eucaristia' | 'liturgia';
-  image: { src: StaticImageData; alt: string; };
+  /**
+   * Image properties
+   */
+  image: { src: StaticImageData | string; alt: string; };
+  /**
+   * Container children orientation.
+   */
   orientation: 'row' | 'row-reverse';
+  /**
+   * Additional style to component elements.
+   */
   style?: { container?: string; text?: string; image?: string; };
 }
 
@@ -19,15 +31,18 @@ function PastoralCard({
     <article
       className={`${style?.container ?? styles.card} ${styles[orientation]}`}
       aria-label={`Notas sobre a pastoral ${title}`}
+      data-testid="pastoral-card"
     >
       <Image
         src={image.src}
         alt={image.alt}
         className={`${styles.card__image} ${style?.image}`}
         aria-hidden
+        data-testid="pastoral-card-image"
       />
       <Typography
         className={`${styles.card__text} ${style?.text}`}
+        testid="pastoral-card-title"
       >
         {title}
       </Typography>
