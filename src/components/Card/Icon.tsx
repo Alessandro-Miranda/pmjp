093025190db605f907b7ad69c0d4f2892e2c53dc
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-04 21:38:47 -0300

    fix: add generic type to createElement function to provide data-testid attribute to component

--- src/components/Card/Icon.tsx
+++ src/components/Card/Icon.tsx
@@ -2,7 +2,14 @@ import Image, { ImageProps } from 'next/image';
 import React from 'react';
 
 function Icon({ ...props }: ImageProps) {
-  return React.createElement(Image, { ...props, 'aria-hidden': true });
+  return React.createElement<ImageProps & { 'data-testid'?: string }>(
+    Image,
+    {
+      ...props,
+      'aria-hidden': true,
+      'data-testid': 'card-icon',
+    },
+  );
 }
 
 export default Icon;
