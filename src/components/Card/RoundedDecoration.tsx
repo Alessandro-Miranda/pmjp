cfd759822051a610d4145b1983ff27e4f32e38a9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-07 22:22:57 -0300

    refact: change animation delay

--- src/components/Card/RoundedDecoration.tsx
+++ src/components/Card/RoundedDecoration.tsx
@@ -12,7 +12,7 @@ function RoundedDecoration({ className }: Props) {
   const drawDecoration: AnimationProps['variants'] = {
     hidden: { pathLength: 0, opacity: 0 },
     visible: (i) => {
-      const delay = 1 + i * 0.5;
+      const delay = i * 0.5;
       return {
         pathLength: 1,
         opacity: 1,
