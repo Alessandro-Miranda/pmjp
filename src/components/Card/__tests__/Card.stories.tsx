74023f55b3bf83e15ddba99262c0aedf8e989db5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-09 11:39:19 -0300

    test(storybook): create stories to RoundeDecoration

--- src/components/Card/__tests__/Card.stories.tsx
+++ src/components/Card/__tests__/Card.stories.tsx
@@ -1,4 +1,5 @@
 import Typography from '@Components/Typography';
+import MassIcon from '@Icons/mass.svg';
 import Welcomed from '@Images/welcomed.jpg';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import * as Card from '..';
@@ -10,7 +11,7 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'Composable component which exports a Root and Icon as sub-components to give granular access to each component part. The Card.Icon has aria-hidden attribute true as default',
+        component: 'Composable component which exports a Root, Icon and a RoundedDecoration as sub-components to give granular access to each component part. The Card.Icon has aria-hidden attribute true as default',
       },
     },
   },
@@ -18,17 +19,42 @@ export default {
 
 const Template: ComponentStory<typeof Card.Root> = (args) => <Card.Root {...args} />;
 
-export const CompleteCard = Template.bind({});
+export const PastoralCard = Template.bind({});
 
-CompleteCard.args = {
+PastoralCard.args = {
   className: styles.card,
   style: {
-    maxWidth: '400px',
+    maxWidth: '300px',
   },
   children: (
     <>
       <Card.Icon src={Welcomed} alt="image test" className={styles.card__image} />
       <Typography className={styles.card__text}>Text test</Typography>
     </>
+  ),
+};
+
+export const ScheduleCard = Template.bind({});
+
+ScheduleCard.args = {
+  children: (
+    <div className={styles.section__schedules__card__container}>
+      <Card.Root
+        className={styles.section__schedules__card}
+        aria-label="Horário de missas e celebrações"
+      >
+        <Card.Icon
+          src={MassIcon}
+          alt="Horário das missas"
+          loading="lazy"
+        />
+        <Typography>
+          missas
+        </Typography>
+      </Card.Root>
+      <Card.RoundedDecoration
+        className={styles.section__schedules__card__rounded__decoration}
+      />
+    </div>
   ),
 };
