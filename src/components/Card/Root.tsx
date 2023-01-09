1c6a25353f793f2c6c5d87b3358367c07568b4d5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-09 11:24:36 -0300

    refact: add default data-testid attribute to component

--- src/components/Card/Root.tsx
+++ src/components/Card/Root.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+  /**
+   * React children that will be displayed
+   */
   children: React.ReactNode;
+  /**
+   * Optional data used only by tests. Default value is "card-root"
+   */
   'data-testid'?: string;
 }
 
 function Root({ children, ...props }: Props) {
-  return React.createElement('article', { ...props, 'data-testid': 'card-root' }, children);
+  return React.createElement('article', { ...props }, children);
 }
 
-Root.defaultProps = { 'data-testid': null };
+Root.defaultProps = { 'data-testid': 'card-root' };
 
 export default Root;
