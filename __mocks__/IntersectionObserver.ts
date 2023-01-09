0203da554bbbe74d3becb1022fbc12a39b98649f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2023-01-09 11:21:08 -0300

    test: update mock

--- __mocks__/IntersectionObserver.ts
+++ __mocks__/IntersectionObserver.ts
@@ -1,27 +1,10 @@
 export default class IntersectionObserverMock {
-  readonly root: Element | null;
-  readonly rootMargin: string;
-  readonly thresholds: ReadonlyArray<number>;
-
-  constructor() {
-    this.root = null;
-    this.rootMargin = '';
-    this.thresholds = [];
-  }
+  get root() { return null };
+  get rootMargin() { return null };
+  get thresholds() { return null };
 
   disconnect() {}
   observe() {}
-  takeRecords(): IntersectionObserverEntry[] {
-    const rectMock = { x: 0, y: 0, width: 0, height: 0, bottom: 0, left: 0, right: 0, toJSON: () => '', top: 0 };
-    return [{
-      boundingClientRect: rectMock,
-      isIntersecting: true,
-      intersectionRatio: 0,
-      intersectionRect: rectMock,
-      rootBounds: null,
-      target: document.querySelector('[data-testid="card-rounded-decoration"]') as HTMLElement,
-      time: 0
-    }]
-  }
+  takeRecords(): IntersectionObserverEntry[] { return [] }
   unobserve() {}
 };
